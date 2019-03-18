3dd3ef061cfd8ef072970562c034dab94d25f2af
kavish4746 <kavishshah84@gmail.com>
2019-03-18 15:35:31 +0530

    updated

--- src/app/services/pastorder.service.ts
+++ src/app/services/pastorder.service.ts
@@ -8,6 +8,7 @@ import { bill_class } from '../Classes/bill';
 export class PastorderService {
   private pastorder:string="http://Localhost:3000/pastorder/";
   private pastorderdetail:string="http://Localhost:3000/pastorderdetail/";
+  private pastorderdetailbyemail:string="http://Localhost:3000/billdetailbyemail/";
   constructor(private _http:HttpClient) { }
   
   getbillrecord(email_id)
@@ -19,5 +20,10 @@ export class PastorderService {
   {
     console.log(fk_bill_id);
     return this._http.get(this.pastorderdetail+fk_bill_id);
+  }
+  getbilldetailsbyemail(fk_email_id)
+  {
+    console.log(fk_email_id);
+    return this._http.get(this.pastorderdetailbyemail+fk_email_id);
   }
 }
